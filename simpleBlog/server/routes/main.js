0650794a05c3b2d2b2ef7df943c1139a9d9c36ea
seigfred0 <saysonseigfred@gmail.com>
2024-01-31 11:24:11 +0800

    update

--- simpleBlog/server/routes/main.js
+++ simpleBlog/server/routes/main.js
@@ -4,6 +4,51 @@ const Post = require('../models/Post')
 
 // Routes
 router.get('/', async (req, res) => {
+    try {
+        const locals = {
+            title: "NodeJs Blog",
+            description: "Simple Blog created with NodeJs & MongoDB"
+        };
+
+        let perPage = 10;
+        let page = req.query.page || 1;
+
+        const data = await Post.aggregate([ { $sort: { createdAt: -1 }} ]).skip(perPage * page - perPage).limit(perPage).exec();
+
+        const count = await Post.countDocuments();
+        const nextPage = parseInt(page) + 1;
+        const hasNextPage = nextPage <= Math.ceil(count / perPage);
+
+       
+        res.render('index', {locals, data, current: page, nextPage: hasNextPage ? nextPage : null});
+    } catch (error) {
+        console.log(error);
+    }
+
+});
+
+
+// router.get('/', async (req, res) => {
+//     const locals = {
+//         title: "NodeJs Blog",
+//         description: "Simple Blog created with NodeJs & MongoDB"
+//     }
+
+//     // retrieving and displaying data
+
+//     try {
+//         const data = await Post.find();
+//         res.render('index', {locals, data});
+        
+//     } catch (error) {
+//         console.log(error);
+//     }
+
+// });
+
+
+
+router.get('/post/:id', async (req, res) => {
     const locals = {
         title: "NodeJs Blog",
         description: "Simple Blog created with NodeJs & MongoDB"
@@ -20,6 +65,8 @@ router.get('/', async (req, res) => {
     }
 
 });
+
+
 
 
 // function insertPostData () {
