3e9e0b7c80b189e39343e01b4e4962c1533f1fcb
seigfred0 <saysonseigfred@gmail.com>
2024-02-01 19:18:27 +0800

    jwt added

--- simpleBlog/server/routes/admin.js
+++ simpleBlog/server/routes/admin.js
@@ -5,7 +5,38 @@ const User = require('../models/User');
 const bcrypt = require('bcrypt'); // encrypt and decrypt passwords
 const jwt = require('jsonwebtoken'); // helps with the cookie
 
-const adminLayout = '../views/layouts/admin'
+const adminLayout = '../views/layouts/admin';
+const jwtSecret = process.env.JWT_SECRET;
+
+
+
+/*
+-- 
+-- Check Login
+*/
+const authMiddleware = (req, res, next) => {
+    const token = req.cookies.token;
+
+    if(!token) {
+        return res.status(401).json({ message: 'Unauthorized '});
+    }
+
+    try {
+        const decoded = jwt.verify(token, jwtSecret);
+        req.userId = decoded.userId;
+        next();
+    } catch (error) {
+        return res.status(401).json({ message: 'Unauthorized '});
+
+        
+    }
+}
+
+
+
+
+
+
 
 router.get('/admin', async (req, res) => {
     try {
@@ -31,15 +62,46 @@ router.post('/admin', async (req, res) => {
     try {
     
         const { username, password } = req.body;
-        console.log(req.body)
-    
-        res.render('admin/index', {layout: adminLayout});
+        const user = await User.findOne({ username });
+
+        if(!user) {
+            return res.status(401).json({ message: 'Invalid Credentials' });
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if(!isPasswordValid) {
+            return res.status(401).json({ message: 'Invalid Credentials' });
+        }
+
+        const token = jwt.sign({ userId: user._id }, jwtSecret);
+        res.cookie('token', token, {httpOnly: true});
+
+        res.redirect('/dashboard')
         
     } catch (error) {
         console.log(error);
     }
 
 });
+
+
+
+
+
+router.get('/dashboard', authMiddleware, async (req, res) => {
+    
+
+    res.render('admin/dashboard')
+});
+
+
+
+
+
+
+
+
+
 
 
 /*
@@ -51,9 +113,20 @@ router.post('/register', async (req, res) => {
     try {
     
         const { username, password } = req.body;
-        console.log(req.body)
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        try {
+            const user = await User.create({ username, password: hashedPassword });
+            res.status(201).json({ message: 'User created', user })
+        } catch (error) {
+            if(error.code === 11000) {
+                res.status(409).json({ message: 'User already created' });
+            }
+            res.status(500).json({ message: 'Internal Server Error' });
+        }
     
-        res.render('admin/index', {layout: adminLayout});  
+        
+
     } catch (error) {
         console.log(error);
     }
@@ -61,3 +134,18 @@ router.post('/register', async (req, res) => {
 });
 
 module.exports = router;
+
+
+// router.post('/admin', async (req, res) => {
+//     try {
+    
+//         const { username, password } = req.body;
+//         console.log(req.body)
+    
+//         res.render('admin/index', {layout: adminLayout});
+        
+//     } catch (error) {
+//         console.log(error);
+//     }
+
+// });
